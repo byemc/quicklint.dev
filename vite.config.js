e8447648ca002cdb96d74852590860448977bbc8
Bye <bye@byecorps.com>
2025-07-13 21:45:09 +0100

    Refactor project to use Vite instead of Parcel

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import legacy from '@vitejs/plugin-legacy'
+
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default {
+    plugins: [
+        legacy({
+            targets: ["defaults", "> 0.05%", "last 2 versions", "Explorer >= 11", "not dead"],
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                xml: resolve(__dirname, 'xml/index.html'),
+            },
+        },
+    },
+
+}
